44699c53cf8a2650469ab1c5971939ec96a81f1c
Amirali Esfandiari <amiralinull@gmail.com>
2024-05-21 01:50:35 +0330

    fix

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-window.addEventListener('load', function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const authority = urlParams.get('Authority');
-    const status = urlParams.get('Status').toLowerCase();
-    const element = document.getElementById('statusMessage');
-    if (status === 'ok') {
-        element.innerHTML = 'پرداخت با موفقیت انجام شد.';
-        element.classList.add('success');
-    } else {
-        element.innerHTML = 'پرداخت با خطا مواجه شد.';
-        element.classList.add('error');
-    }
+window.addEventListener("load", function () {
+  const urlParams = new URLSearchParams(window.location.search);
+  const authority = urlParams.get("Authority");
+  const status = urlParams.get("Status").toLowerCase();
+  const element = document.getElementById("statusMessage");
+  if (status === "ok") {
+    element.innerHTML = "پرداخت با موفقیت انجام شد.";
+    element.classList.add("success");
+  } else {
+    element.innerHTML = "پرداخت با خطا مواجه شد.";
+    element.classList.add("error");
+  }
 
-    let bot_link = this.document.getElementById('bot-link');
-    bot_link.href = 'https://t.me/arzeshmand_bot?start=pay_' + authority;
+  let bot_link = this.document.getElementById("bot-link");
+  bot_link.href = "https://t.me/marveledu_bot?start=pay_" + authority;
 });
+
